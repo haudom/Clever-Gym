a96e12dc8686ddc0bc474688eda3d0ed5e0b698f
Christian Schäfer <14093936+LonelessCodes@users.noreply.github.com>
2022-06-07 19:34:56 +0200

    Update settings.storage.js

--- src/store/settings.storage.js
+++ src/store/settings.storage.js
@@ -1,12 +1,25 @@
 import { defineStore } from "pinia";
 
 export const useSettingsStore = defineStore("settings", {
+  /**
+   * @returns {{ weight: number, age: number, name: string }}
+   */
   state: () => ({
     weight: null,
     name: null,
     age: null,
+    // überschreibe die Standardwerte mit denen aus LocalStorage, falls verfügbar, ansonsten
+    // übernehme einfach die properties aus einem leeren Objekt.
+    ...(localStorage.getItem("settings") ?? {})
   }),
   actions: {
+    /**
+     * Setze die Nutzereinstellungen.
+     * @param {object} settings 
+     * @param {string} settings.name 
+     * @param {number} settings.age
+     * @param {number} settings.weight 
+     */
     setSettings({ name, age, weight }) {
       this.name = name;
       this.age = age;
