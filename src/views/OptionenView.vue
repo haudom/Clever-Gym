a1a2e3fddf3deff1bf218a4260d00e97ecf3996b
ÂDominik Hauger <s82776@htw-dresden.de>
2022-06-11 22:45:57 +0200

    Einstellungen implementiert

--- src/views/OptionenView.vue
+++ src/views/OptionenView.vue
@@ -1,5 +1,22 @@
 <template>
+<h1>Einstellungen</h1>
+<div>
+  Name
+  <input type="text" >
+  </div>
   <div>
-    
+    Gewichte
+
+    <input type="text" value="70">kg
+  </div>
+  <div>
+    Geburtsdatum
+    <input type="date">
   </div>
 </template>
+
+<style>
+input{
+  margin: 5px;
+}
+</style>
